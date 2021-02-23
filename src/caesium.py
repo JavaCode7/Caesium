0c794afd4f98200ee4b74ec6c62b43c59cb48546
Pranav <70764593+pranavbaburaj@users.noreply.github.com>
2021-02-23 14:38:48 +0530

    Added filename validation

--- src/caesium.py
+++ src/caesium.py
@@ -1,36 +1,62 @@
-import caelex, sys, os, caeparser, caei
+import caelex, sys, os, caeparser, caei, caeerr
 
-try:
-    with open(str(sys.argv[1])) as fn:
-        script = fn.read()
-except IndexError:
-    print("Welcome to Caesium interactive shell!")
-    while True:
-        try:
-            a = input('> ')
-        except KeyboardInterrupt:
-            print("\n\033[3m KeyboardInterrupt \033[0m")
-            continue
-        if a == ".exit":
-            break
-        elif a == "clear" or a == "cls" or a == "clean":
-            os.system("cls" if os.name != "posix" else "clear")
-        else:
-            lexer = caelex.CaeLexer(a)
+class ArgumentParser:
+    def __init__(self, arguments):
+        self.arguments = arguments[1:]
 
-            lexed = lexer.lex()
+        self.parse_arguments()
 
-            print(lexed)
+    def parse_arguments(self):
+        # print(self.arguments)
+        if len(self.arguments) >= 1:
+            filename = self.arguments[0]
+            if os.path.exists(filename):
+                if not os.path.isfile(filename):
+                    self.throw_error(f"{filename} is not a file")
+                else:
+                    pass
+            else:
+                self.throw_error(f"{filename} does not exists")
 
-            parser = caeparser.CaeParser(lexed)
+    def throw_error(self, error_message):
+        print(f"ERROR : {error_message}")
+        sys.exit()
 
-            parser.parse()
 
-            print(parser.ast)
+# try:
+#     with open(str(sys.argv[1])) as fn:
+        
+#         script = fn.read()
+# except IndexError:
+#     print("Welcome to Caesium interactive shell!")
+#     while True:
+#         try:
+#             a = input('> ')
+#         except KeyboardInterrupt:
+#             print("\n\033[3m KeyboardInterrupt \033[0m")
+#             continue
+#         if a == ".exit":
+#             break
+#         elif a == "clear" or a == "cls" or a == "clean":
+#             os.system("cls" if os.name != "posix" else "clear")
+#         else:
+#             lexer = caelex.CaeLexer(a)
 
-            interp = caei.CaeInterpreter()
+#             lexed = lexer.lex()
 
-            behavior = interp.visit(parser.ast[0])
+#             print(lexed)
 
-            print(behavior)
-            print(type(behavior))
+#             parser = caeparser.CaeParser(lexed)
+
+#             parser.parse()
+
+#             print(parser.ast)
+
+#             interp = caei.CaeInterpreter()
+
+#             behavior = interp.visit(parser.ast[0])
+
+#             print(behavior)
+#             print(type(behavior))
+if __name__ == '__main__':
+    argument_parser = ArgumentParser(sys.argv)
