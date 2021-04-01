ab92edc48f136828e79655224d199749c6e955f8
JavaCode7 <76915005+JavaCode7@users.noreply.github.com>
2021-04-01 12:47:57 +0100

    Restart Caesium

--- src/caetoken.py
+++
@@ -1,8 +0,0 @@
-class CaeToken():
-
-    def __init__(self, _type: str = "", value = ""):
-        self.type: str = _type
-        self.value = value
-    
-    def __repr__(self):
-        return f"Token of type {self.type} and value '{self.value}'"
